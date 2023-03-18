c14c0ef769141e713942bf3bd6526466e247c524
parth448812 <122322634+parth448812@users.noreply.github.com>
2023-03-17 20:55:00 -0400

    Client Project and Font Update

--- itis3135/scripts/polygons.js
+++ itis3135/scripts/polygons.js
@@ -4,43 +4,43 @@ function getShape(num)
     switch (num) 
     {
         case 1:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Henagon")
             break
         case 2:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Digon")
             break
         case 3:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Trigon")
             break;
         case 4:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Tetragon")
             break
         case 5:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Pentagon")
             break
         case 6:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Hexagon")
             break
         case 7:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Heptagon")
             break
         case 8:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Octagon")
             break
         case 9:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Enneagon")
             break
         case 10:
-            document.querySelector("#info")
+            document.querySelector("#alrt")
             alert("Decagon")
             break
     }
