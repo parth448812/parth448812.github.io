249b27db45d63b675bf93e566aa32b1c31c80890
parth448812 <122322634+parth448812@users.noreply.github.com>
2023-04-14 18:59:05 -0400

    Activity 15 and Site Evaluations Update

--- itis3135/scripts/siteLinks.js
+++ itis3135/scripts/siteLinks.js
@@ -10,28 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
         <a href="contract.html">Contract</a> |
         <a href="tables.html">Tables</a> |
         <a href="forms.html">Forms</a> |
-        <a href="website_evaluations.html">Site Evaluation</a> |
-        <a href="firstscripts.html">First Scripts</a> |
+        <a href="website_evaluations.html">Site&nbspEvaluation</a> |
+        <a href="firstscripts.html">First&nbspScripts</a> |
         <a href="calculator_try.html">Calculator Try</a> |
         <a href="calculator_fcc.html">Calculator FCC</a> |
         <a href="polygons.html">Polygons</a> |
         <a href="arrays.html">Arrays</a> |
         <a href="slideshow.html">Slideshow</a> |
-        <a href="project1.html">Project Design</a> |
-        <a href="review1.html">Peer Review 1</a> |
-        <a href="review2.html">Peer Review 2</a>
+        <a href="project1.html">Project&nbspDesign</a> |
+        <a href="review1.html">Peer&nbspReview&nbsp1</a> |
+        <a href="review2.html">Peer&nbspReview&nbsp2</a>
       </nav>
       <br>
       <nav class="menu">
-        <a href="stuff/H0Ri%20bLeW3b%24iTe.html">CRAPPY Site</a> |
+        <a href="stuff/H0Ri%20bLeW3b%24iTe.html">CRAPPY&nbspSite</a> |
         <a href="accessibility/Patel-Activity_Accessibility.html">Accessibility</a> |
         <a href="hobby">Hobby</a> |
-        <a href="Patel-Activity11/Patel-Activity11.html">Accordion Widget</a> |
-        <a href="Patel-Activity12/Patel-Activity12.html">bxSlider Plugin</a> |
-        <a href="Patel-Activity13/Patel-Activity13.html">XML to JSON</a> |
-        <a href="Patel-Activity14/Patel-Activity14.html">Load Speaker Files</a> |
-
-        <a href="client_project">Client Project</a>
+        <a href="Patel-Activity11/Patel-Activity11.html">Accordion&nbspWidget</a> |
+        <a href="Patel-Activity12/Patel-Activity12.html">bxSlider&nbspPlugin</a> |
+        <a href="Patel-Activity13/Patel-Activity13.html">XML&nbspto&nbspJSON</a> |
+        <a href="Patel-Activity14/Patel-Activity14.html">Load&nbspSpeaker&nbspFiles</a> |
+        <a href="Patel-Activity15/Patel-Activity15.html">Use&nbspExpanded&nbspJSON&nbspFile</a> |
+        <a href="client_project">Client&nbspProject</a>
         </nav>
     `;
     document.body.insertBefore(header, document.body.firstChild);
