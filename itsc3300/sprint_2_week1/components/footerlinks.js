68bad4f7802fd67a2d123f7c0654c3696de4235f
parth448812 <parthkpatel04@gmail.com>
2023-11-05 21:33:11 -0500

    itis3300 update

---
+++ itsc3300/sprint_2_week1/components/footerlinks.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var footer = document.createElement("footer");
+  
+    footer.innerHTML = `
+        <p>Designed by <a>Group 4 </a>&copy;2023</p>
+    `;
+  
+    document.body.appendChild(footer);
+  });  
